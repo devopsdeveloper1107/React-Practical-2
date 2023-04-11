2d57d7a82e6cb49604a9b70d21c336f42c89e6b9
Devops Developer <100074939+devopsdeveloper1107@users.noreply.github.com>
2023-04-11 08:28:18 +0530

    Add files via upload

--- src/Components/Common/Header.js
+++ src/Components/Common/Header.js
@@ -9,10 +9,16 @@ function Header()
                 <div className="container">
                 <div className="row">
                     <div className="col-md-12">
-                        <h5 className="mt-0">Left Menu</h5>
+                        <h5 className="mt-0">Left Menu</h5>                    
                         <ul className="navbar-nav">
                             <li className="nav-item"><Link to="/" className="nav-link">Home</Link></li>
                             <li className="nav-item"><Link to="/userdata" className="nav-link">User Data List</Link></li>
+                            <li className="nav-item"><Link to="/exportcsv" className="nav-link">Export Csv</Link></li>
+                            <li className="nav-item"><Link to="/enablebutton" className="nav-link">Enable Button</Link></li>
+                            <li className="nav-item"><Link to="/addmoreinput" className="nav-link">Add More Input</Link></li>
+                            
+                            
+                        
                         </ul>
 
                     </div>
