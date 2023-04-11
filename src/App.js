f90cdc07de655119211a8c3950ea3c36cd8be009
Devops Developer <100074939+devopsdeveloper1107@users.noreply.github.com>
2023-04-11 08:27:38 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
+import React ,{  } from "react";
 import {Routes, Route} from 'react-router-dom';
 import Header from './Components/Common/Header';
 import Home from './Components/Home';
 import Userdata from './Components/Userdata';
+import Adduser from "./Components/Adduser";
+import Edituser from "./Components/Edituser";
+import ExportCsv from "./Components/Exportcsv";
+import Enablebutton from "./Components/Enablebutton";
+import Addmoreinput from "./Components/Addmoreinput";
 
-function App() {
-  return (
+
+
+function App() 
+{  
+  return (    
     <div className="App">
-     <div className='leftPanel'>
+     <div className='leftPanel'>    
       <Header/>
      </div>
      <div className='rightPanel'>
       <Routes>
-      <Route path='/' element={<Home/>} />  
+      <Route path='/' element={<Home/>} /> 
       <Route path='/userdata' element={<Userdata/>} />  
-      </Routes>
-      
+      <Route path='/adduser' element={<Adduser/>} />  
+      <Route path='/edituser/:id' element={<Edituser/>} /> 
+      <Route path='/exportcsv' element={<ExportCsv/>} /> 
+      <Route path='/enablebutton' element={<Enablebutton/>} /> 
+      <Route path='/addmoreinput' element={<Addmoreinput/>} /> 
+    
+     
+          
+
+      </Routes> 
+          
      </div>
     </div>
   );
