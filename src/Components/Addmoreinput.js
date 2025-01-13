7ac2e9be505af8bb86d2b48f0ee82be2a6f7721a
Devops Developer <100074939+devopsdeveloper1107@users.noreply.github.com>
2025-01-13 20:36:09 +0530

    Update Addmoreinput.js

--- src/Components/Addmoreinput.js
+++ src/Components/Addmoreinput.js
@@ -1,82 +1,117 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from "react";
 
-function Addmoreinput()
-{
-  const[formValues, setFormValues]= useState([{name:'', email:'', address:''}]);
-  const[msg, setMsg]= useState('');
+function Addmoreinput() {
+ const[formvalue,setFormValue]=useState([{id:0,name:'',email:'',address:''}]);
+ const [msg,setMsg]=useState('');
+ const[isSubmitDisabled, setIsSubmitDisabled]= useState(true);
 
-  const handleInputChange=(index, event)=>{
-    let data= [...formValues];
-    data[index][event.target.name]= event.target.value;
-    setFormValues(data);
-  }
-const addFields=()=>{
-    let addField= {name:'', email:'', address:''};
-    setFormValues([...formValues, addField])
+ const addFields=()=>{
+  const newId=formvalue.length;
+  setFormValue([...formvalue,{id:newId,name:'',email:'',address:''}]);
+ }
+
+ const handleInput=(index,event)=>{
+  const data= [...formvalue];
+  data[index][event.target.name]= event.target.value;
+  setFormValue(data);
+ }
+ const removeFields=(index)=>{
+ const removedata=formvalue.filter((_, i)=>i!==index);
+ setFormValue(removedata);
+ }
+
+ const checkifanyvalue =useCallback(()=>{
+ return formvalue.some((row)=>row.name.trim()!=='' || row.email.trim()!=='' || row.address.trim()!=='')
+ },[formvalue]);
+
+ useEffect(()=>{
+  setIsSubmitDisabled(!checkifanyvalue())
+ },[checkifanyvalue])
+
+ const handelSubmit=(e)=>{
+  e.preventDefault();
+  console.log("submit data", formvalue)
+  setMsg("Data saved successfully");
+ }
+  return (
+    <React.Fragment>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h5 className="mt-3 mb-3">
+              {" "}
+              Add Remove Multiple User Input fields{" "}
+            </h5>
+            <button className="btn btn-success btn-lg mb-3" onClick={addFields}>Add More</button>
+            <table className="table table-bordered">
+              <thead>
+                <tr>
+                  <th>Sr. No</th>
+                  <th>User Name</th>
+                  <th>Email</th>
+                  <th>Address</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody> 
+                {formvalue?.map((udata,index)=>(
+
+                                       
+                <tr key={index}>
+                  <td>{index+1}</td>
+                  <td>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="name"
+                      placeholder="Enter Username" 
+                      value={udata.name} 
+                     onChange={(event)=>handleInput(index,event)}  
+                                     
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="email"
+                      placeholder="Enter Email"  
+                      value={udata.email} 
+                     onChange={(event)=>handleInput(index,event)}                      
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="address"
+                      placeholder="Enter Address"
+                      value={udata.address} 
+                     onChange={(event)=>handleInput(index,event)}                        
+                    />
+                  </td>
+                  <td>  
+                    {index!==0 &&(
+                   <button className="btn btn-danger mx-2" onClick={()=>removeFields(index)}>Remove</button>  
+                    )}                 
+                                  
+                  </td>
+                </tr>
+             ))}  
+              </tbody>
+            </table>
+            <button className="btn btn-success btn-lg"
+            disabled={isSubmitDisabled}
+            onClick={handelSubmit}
+            >Submit</button>
+            {msg && (
+              <p className="mt-3 text-success">{msg}</p>
+            )}
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 
-const removeFields=(index)=>{
- let data= [...formValues];
- data.splice(index,1);
- setFormValues(data);
-}
-
-const handleSubmit=(e)=>{
-    e.preventDefault();
-    console.log(formValues);
-    // axios.post("", formValues);
-    setMsg("Data Saved Successfully");
-
-}
-
-    return(
-        <React.Fragment>
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12">                       
-                    <div className="col-md-12">
-                    <h5 className='mt-3 mb-3'>Add Multiple Users</h5>                       
-                    <button className="btn btn-success btn-lg mb-3" onClick={ addFields}>Add More </button>             
-                    <table className="table table-bordered">
-                    <thead>                        
-                    <tr>                       
-                    <th>Sr. No</th>
-                    <th>User Name</th>
-                    <th>Email</th>
-                    <th>Address</th>
-                    <th>Action</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            formValues.map( (input, index)=>
-                            <tr key={index}>                        
-                    <td>{index+1} </td>
-                    <td><input type="text"  className="form-control" name="name" value={formValues.name} onChange={ event=>handleInputChange(index,event)} placeholder="Enter Username"/>  </td>
-                    <td><input type="text" className="form-control" name="email"   value={formValues.email} onChange={ event=>handleInputChange(index,event)} placeholder="Enter email"/></td>
-                    <td><input type="text" className="form-control" name="address" value={formValues.address} onChange={ event=>handleInputChange(index,event)} placeholder="Enter Address"/></td>
-                    <td>  
-                        {
-                          index!==0 &&(
-                            <button className="btn btn-danger mx-2" onClick={ ()=>removeFields(index)}>Remove </button>
-                          )  
-                        }                                       
-                                          
-                    </td>
-                    </tr> 
-                            )
-                        }                              
-                                          
-                    </tbody>
-                    </table>
-                    <button className="btn btn-success btn-lg" onClick={ handleSubmit}>Submit </button>
-                </div>
-            
-                </div>
-                </div>
-                </div>
-
-        </React.Fragment>
-    );
-}
 export default Addmoreinput;
